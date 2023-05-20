872474ad7e44205e900892cdb69264020aabaa4c
abh1sh3kk <abhishekacharya171@gmail.com>
2023-05-20 21:31:19 +0545

    Temporarily change connection string

--- Server/db.js
+++ Server/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 (async() => {
 	try {
-		await mongoose.connect(process.env.CONN_STRING)
+		await mongoose.connect("mongodb+srv://abh1sh3k:su%25LA%24ineg65@production-cluster-mumb.zzi5bq5.mongodb.net/")
 		console.log("Connection Established")
 	}
 	catch(e) {
