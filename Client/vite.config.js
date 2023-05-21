a3080aac1b4edcc4c60d6fcc967b7fdf32ace2af
abh1sh3kk <abhishekacharya171@gmail.com>
2023-05-21 13:38:46 +0545

    restore vite config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,24 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxInject: `import React from 'react'`,
-  },
-  resolve: {
-    alias: {
-      'react/jsx-runtime': 'react/jsx-runtime.js',
-      'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
-    },
-  },
 })
